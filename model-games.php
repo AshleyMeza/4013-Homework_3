04cb2105037b8255ec6f428954b58f2e47c63bae
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-04 19:23:31 -0500

    Update model-games.php

--- model-games.php
+++ model-games.php
@@ -2,7 +2,7 @@
 function selectGames() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT game_id,winner,score,date FROM `games`;");
+        $stmt = $conn->prepare("SELECT game_id,winner,loser,score,date FROM `games`;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
