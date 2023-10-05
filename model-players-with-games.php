d6c22acef9b03fc5cb4e84791c1ee30c95a1a5ab
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-04 20:25:30 -0500

    Update model-players-with-games.php

--- model-players-with-games.php
+++ model-players-with-games.php
@@ -12,4 +12,19 @@ function selectPlayers() {
         throw $e;
     }
 }
+
+function selectGamesbyPlayers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("PUT THING HERE");
+        $stmt->bind_param("p", $pid); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
