f3910cfd72efe9d30cc82236f48d9135ba0a49d1
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-04 20:11:04 -0500

    Create view-clubs-by-games.php

---
+++ view-clubs-by-games.php
@@ -0,0 +1,33 @@
+<h1>Games Review by Clubs</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Game ID</th>
+      <th>Winner</th>
+      <th>Loser</th>
+      <th>Score</th>
+      <th>Date</th>
+      <th>Club Name</th>
+      <th>Location</th> 
+      </tr>
+      <tbody>
+  <?php
+  while ($club = $clubs -> fetch_assoc()) {
+    ?>
+    <tr>
+      <td><?php echo $game['game_id'];?></td>
+      <td><?php echo $game['winner'];?></td>
+      <td><?php echo $game['loser'];?></td>
+      <td><?php echo $game['score'];?></td>
+      <td><?php echo $game['date'];?></td>
+      <td><?php echo $game['club_name'];?></td>
+      <td><?php echo $game['location'];?></td>
+    </tr>
+    <?php
+  }
+  ?>
+      </tbody>
+    </thead>
+  </table>
+</div>
