471da6ffef27840f77e79be8c62c14c00f0ac3f8
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-11 14:27:25 -0500

    Update clubs.php

--- clubs.php
+++ clubs.php
@@ -3,7 +3,7 @@ require_once ("util-db.php");
 require_once ("model-clubs.php");
 
 $pageTitle = "Clubs";
-include "view-clubs.php";
+include "view-header.php";
 
 $clubs = selectClubs();
 include "view-clubs.php";
