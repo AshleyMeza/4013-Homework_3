7d6709bfa994c32c4c0c56df33c654afe9ec67b7
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-11 13:48:38 -0500

    Create model-clubs.php

---
+++ model-clubs.php
@@ -0,0 +1,15 @@
+<?php
+function selectClubs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT club_id, club_name,location,color(s) FROM `club`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
