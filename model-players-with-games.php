0df27d61a99168828070dc46d7dda85b1cf4e98e
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-16 14:31:30 -0500

    Update model-players-with-games.php

--- model-players-with-games.php
+++ model-players-with-games.php
@@ -16,7 +16,7 @@ function selectPlayers() {
 function selectGamesbyPlayers($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT g.game_id,winner,loser,score,date,stadium_name,s.location FROM `games` g JOIN stadium s ON g.stadium_id = s.stadium_id WHERE g.game_id=?");
+        $stmt = $conn->prepare("SELECT g.game_id,winner,loser,score,date,stadium_name,s.location FROM `games` g JOIN stadium s ON g.stadium_id = s.stadium_id WHERE g.player_id=?");
         $stmt->bind_param("i", $pid); 
         $stmt->execute();
         $result = $stmt->get_result();
