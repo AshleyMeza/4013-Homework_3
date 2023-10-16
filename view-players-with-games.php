31ac146cecc85f02e4d380e6015ed4ef7f7b4591
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-16 13:59:37 -0500

    Update view-players-with-games.php

--- view-players-with-games.php
+++ view-players-with-games.php
@@ -1,19 +1,20 @@
 <h1>Players with Games</h1>
 <div class="card-group">
   <?php
-  while ($player = $players -> fetch_assoc()) {
+  while ($player = $players -> fetch_assoc()) 
+  {
     ?>
     <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $player['player_name'];?></h5>
-      <p class="card-text">
-    <ul class="list-group">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $player['player_name'];?></h5>
+          <p class="card-text">
+            <ul class="list-group">
         
       <?php
         $games = selectGamesbyPlayers($player['player_id']);
     while ($game = $games -> fetch_assoc()){
       ?>
-       <li class="list-group-item"><?php echo $game['winner'];?> - <?php echo $game['loser'];?> - <?php echo $game['score'];?> - <?php echo $game['date'];?></li>      
+       <li class= "list-group-item"><?php echo $game['game_id'];?> - <?php echo $game['winner'];?> - <?php echo $game['loser'];?> - <?php echo $game['score'];?> - <?php echo $game['date'];?></li>      
   <?php
     }
   ?>
