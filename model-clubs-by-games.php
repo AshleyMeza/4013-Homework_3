d2bc7b5e82b87123f63eda7ad957ea7a9119fb9e
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-16 13:54:04 -0500

    Update model-clubs-by-games.php

--- model-clubs-by-games.php
+++ model-clubs-by-games.php
@@ -2,7 +2,7 @@
 function selectClubsbyGames($gid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT g.game_id,winner,loser,score,g.date,club_name,c.location FROM `games` g JOIN club c ON g.club_id = c.club_id WHERE g.player_id = ?;");
+        $stmt = $conn->prepare("SELECT g.game_id,winner,loser,score,g.date,club_name,c.location FROM `games` g JOIN club c ON g.club_id = c.club_id WHERE g.player_id = ?");
         $stmt->bind_param("i",$gid); 
         $stmt->execute();
         $result = $stmt->get_result();
