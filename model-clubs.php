d08eee21b7dcdc1df1755038e547cc925d95ab7a
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 21:18:47 -0500

    Update model-clubs.php

--- model-clubs.php
+++ model-clubs.php
@@ -12,4 +12,47 @@ function selectClubs() {
         throw $e;
     }
 }
+
+function insertClubs($cName,$cLocation,$cColor) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `club` (`club_name`, `location`, `color(s)`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cName,$cLocation,$cColor);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateClubs($cName,$cLocation,$cColor,$cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `club` set `club_name`=?, `location`= ?, `color(s)`= ? where club_id=?");
+        $stmt->bind_param("sssi", $cName,$cLocation,$cColor,$cid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteClubs($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from club where club_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
