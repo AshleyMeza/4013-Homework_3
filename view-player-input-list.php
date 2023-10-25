6c3a30138cc09d67c4acf8220834c7262767ef04
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-25 15:17:08 -0500

    Delete view-player-input-list.php

--- view-player-input-list.php
+++
@@ -1,9 +0,0 @@
-<select class="form-select" id="pName" name="pName">
-  <?php
-    while ($pNameItem =$pNameList->fetch_assoc()){
-      ?>
-      <option value="<?php echo $pNameItem['player_id'];?>"><?php echo $pNameItem['player_name'];?></option>
-  <?php
-    }
-    ?>
-</select>
