27ee308203d93619031b4c730736df570ff8e65e
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-25 15:58:13 -0500

    Update view-game-input-list.php

--- view-game-input-list.php
+++ view-game-input-list.php
@@ -1,8 +1,13 @@
 <select class="form-select" id="gWinner" name="gWinner">
   <?php
     while ($winnerItem =$winnerList->fetch_assoc()){
+      $selText = "";
+      if ($selectedWinner == $gameItem['game_id']){
+        $selText = "selected";
+
+      }
       ?>
-      <option value="<?php echo $winnerItem['game_id'];?>"><?php echo $winnerItem['winner'];?></option>
+      <option value="<?php echo $winnerItem['game_id'];?>"<?=$selText?>><?php echo $winnerItem['winner'];?></option>
   <?php
     }
     ?>
