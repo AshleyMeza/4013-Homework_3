e44a22be5fa4567fb82f46a6f578754c897da9d7
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-25 15:14:55 -0500

    Delete view-club-input-list.php

--- view-club-input-list.php
+++
@@ -1,9 +0,0 @@
-<select class="form-select" id="cName" name="cName">
-  <?php
-    while ($cNameItem =$cNameList->fetch_assoc()){
-      ?>
-      <option value="<?php echo $cNameItem['club_id'];?>"><?php echo $cNameItem['club_name'];?></option>
-  <?php
-    }
-    ?>
-</select>
