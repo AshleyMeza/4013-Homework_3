92ca4b42e26ff9318023ff6bf38bfd983529781d
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 23:22:16 -0500

    Update clubs.php

--- clubs.php
+++ clubs.php
@@ -5,6 +5,32 @@ require_once ("model-clubs.php");
 $pageTitle = "Clubs";
 include "view-header.php";
 
+if (isset($_POST['actionType'])){
+ switch ($_POST['actionType']){
+  case 'Add':
+   if (insertGames($_POST['cName'],$_POST['cLocation'],$_POST['cColor'])){
+    echo '<div class="alert alert-success" role="alert"> Club Added!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
+   break;
+  case 'Edit':
+   if (updateGames($_POST['cName'],$_POST['cLocation'],$_POST['cColor'],$_POST['cid'])){
+    echo '<div class="alert alert-success" role="alert"> Club Updated!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
+   break;
+  case 'Delete':
+   if (deleteGames($_POST['cid'])){
+    echo '<div class="alert alert-success" role="alert"> ClubDeleted!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
+   break;
+ }
+}
+
 $clubs = selectClubs();
 include "view-clubs.php";
 
