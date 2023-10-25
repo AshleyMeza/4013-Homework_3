5b06ff3dc262922bebb2cd59e789af356b60a8fe
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 22:35:57 -0500

    Update games.php

--- games.php
+++ games.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
  switch ($_POST['actionType']){
   case 'Add':
-   insertGames($_POST['gWinner'],$_POST['gLoser'],$_POST['gScore'],$_POST['gDate']);
+   if (insertGames($_POST['gWinner'],$_POST['gLoser'],$_POST['gScore'],$_POST['gDate'])){
+    echo '<div class="alert alert-success" role="alert"> Game Score Added!</div>';
+   } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+   }
    break;
  }
 }
