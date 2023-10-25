f9235effe2e5d1f85d2fa58a41dcbd570d43ad5d
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 22:15:50 -0500

    Update model-games.php

--- model-games.php
+++ model-games.php
@@ -12,4 +12,46 @@ function selectGames() {
         throw $e;
     }
 }
+function insertGames($gWinner,$gLoser,$gScore,$gDate) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `games` (`winner`, `loser`, `score`,'date') VALUES (?, ?, ?,?)");
+        $stmt->bind_param("ssss",$gWinner,$gLoser,$gScore,$gDate);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateGames($gWinner,$gLoser,$gScore,$gDate,$gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `games` set `winner`=?, `loser`= ?, `score`= ?,`date`= ? where game_id=?");
+        $stmt->bind_param("ssssi", $gWinner,$gLoser,$gScore,$gDate,$gid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteGames($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from games where game_id = ?");
+        $stmt->bind_param("i", $gid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
