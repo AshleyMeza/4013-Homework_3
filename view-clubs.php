d380e0f67f82c0049d1f8e6045b218b886bf2453
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-25 00:00:25 -0500

    Update view-clubs.php

--- view-clubs.php
+++ view-clubs.php
@@ -1,4 +1,14 @@
-<h1>Club</h1>
+<div class="row">
+  <div class = "col">
+  <h1>Club</h1>
+  </div>
+  <div class = "col-auto">
+<?php
+include "view-clubs-newform.php";
+?>
+  </div> 
+</div>
+
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -7,6 +17,8 @@
       <th>Club Name</th>
       <th>Location</th>
       <th>Color(s)</th> 
+      <th></th> 
+      <th></th> 
       <th></th> 
       </tr>
       </thead>
@@ -19,6 +31,23 @@
       <td><?php echo $club['club_name'];?></td>
       <td><?php echo $club['location'];?></td>
       <td><?php echo $club['color(s)'];?></td>
+
+      <td>
+        <?php
+          include "view-clubs-editform.php";
+        ?>
+      </td>
+      <td>
+        <form method = "post" action="">
+      <input type="hidden" name= "cid" value ="<?php echo $club['club_id'];?>">
+    <input type=hidden" name ="actionType" value = "Delete"?
+      <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+          </svg>
+       </button>
+        </form>
+    </td>
     </tr>
     <?php
   }
