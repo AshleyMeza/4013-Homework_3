dc7b1ca811afe3404e88c4a8aded214ed21d1fc6
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 22:31:17 -0500

    Update games.php

--- games.php
+++ games.php
@@ -5,6 +5,14 @@ require_once ("model-games.php");
 $pageTitle = "Game scores";
 include "view-header.php";
 
+if (isset($_POST['actionType'])){
+ switch ($_POST['actionType']){
+  case 'Add':
+   insertGames($_POST['gWinner'],$_POST['gLoser'],$_POST['gScore'],$_POST['gDate']);
+   break;
+ }
+}
+
 $games = selectGames();
 include "view-games.php";
 
