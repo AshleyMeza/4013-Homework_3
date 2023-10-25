22317b87d16b53b36ae5247ad1a4dac8f9eb3614
AshleyMeza <111796979+AshleyMeza@users.noreply.github.com>
2023-10-24 23:41:21 -0500

    Update players.php

--- players.php
+++ players.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
  switch ($_POST['actionType']){
   case 'Add':
-   if (insertPlayer($_POST['pName'],$_POST['pNumber'],$_POST['pCleats'])){
+   if (insertPlayers($_POST['pName'],$_POST['pNumber'],$_POST['pCleats'])){
     echo '<div class="alert alert-success" role="alert"> Player Added!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Edit':
-   if (updatePlayer($_POST['pName'],$_POST['pNumber'],$_POST['pCleats'],$_POST['pid'])){
+   if (updatePlayers($_POST['pName'],$_POST['pNumber'],$_POST['pCleats'],$_POST['pid'])){
     echo '<div class="alert alert-success" role="alert"> Player Updated!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
    }
    break;
   case 'Delete':
-   if (deletePlayer($_POST['pid'])){
+   if (deletePlayers($_POST['pid'])){
     echo '<div class="alert alert-success" role="alert"> Player Deleted!</div>';
    } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
